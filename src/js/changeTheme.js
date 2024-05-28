463cb5e4f41668c1ff878f7e0b8ce79b6eeb4433
nestertsovv <n.nestertsovv@gmail.com>
2024-05-28 15:56:43 +0300

    new changes for default settings

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -3,7 +3,14 @@ const refs = {
   select: document.querySelector('.theme-color'),
 };
 
-let color = '#ed3b44';
+const lime = '#c6e327';
+const red = '#ed3b44';
+const blue = '#0041e8';
+const tomato = '#e6533c';
+const green = '#2b4441';
+const orange = '#ff7f08';
+let newColor = lime;
+
 const classBG = '.bg-color';
 const classTextColor = '.text-color';
 
@@ -13,4 +20,42 @@ function onChangeColor(e) {
   const value = e.target.value;
 
   const bgArr = document.querySelectorAll(classBG);
+  const colorArr = document.querySelectorAll(classTextColor);
+
+  switch (value) {
+    case 'lime':
+      newColor = lime;
+      break;
+
+    case 'red':
+      newColor = red;
+      break;
+
+    case 'blue':
+      newColor = blue;
+      break;
+
+    case 'tomato':
+      newColor = tomato;
+      break;
+
+    case 'green':
+      newColor = green;
+      break;
+
+    case 'orange':
+      newColor = orange;
+      break;
+
+    default:
+      break;
+  }
+
+  bgArr.forEach(color => {
+    color.style.setProperty('--red', newColor);
+  });
+
+  colorArr.forEach(color => {
+    color.style.setProperty('--red', newColor);
+  });
 }
