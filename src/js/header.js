cb5f2d4a12b902b7659b77d5fe2707367c677830
Oksanium <164672938+Oksanium@users.noreply.github.com>
2024-05-30 12:28:31 +0200

    Header hero menu oks beta (#16)
    * done
    
    * fix-btn-fsz--Prettify-code

---
+++ src/js/header.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// MENU DESKTOP
+
+const nav = document.querySelector('.js-desk-menu');
+const menu = nav.querySelector('.js-menu-btn');
+const menuList = nav.querySelector('.js-menu-list');
+nav.addEventListener('click', onNavClick);
+
+function onNavClick(evt) {
+    if (evt.target === menu) {
+        menuList.classList.toggle('is-open');
+        return;
+    }
+    if (evt.target.classList.contains('menu-link')) {
+        menuList.classList.remove('is-open');
+    }
+}
+
+// MENU MOBILE
+// open
+const burgerBtn = document.querySelector('.js-burger-btn');
+const mobMenu = document.querySelector('.js-mob-menu');
+burgerBtn.addEventListener('click', onBurgerClick);
+
+function onBurgerClick() {
+    mobMenu.classList.add('is-open');
+}
+// close
+const crossBtn = document.querySelector('.js-cross');
+crossBtn.addEventListener('click', onCrossClick);
+
+function onCrossClick() {
+    mobMenu.classList.remove('is-open');
+}
+
+mobMenu.addEventListener('click', onMobMenuItemClick);
+
+function onMobMenuItemClick(evt) {
+    if (evt.target.nodeName === 'A') {
+        mobMenu.classList.remove('is-open');
+    }
+}
