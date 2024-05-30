869df5089f995e73c29c2c198cd4447ad3fa5293
Oleksandr Zhynkin <godinsighted@gmail.com>
2024-05-30 17:14:30 +0300

    modal finished (#14)
    * start
    
    * html/css
    
    * h
    
    * modal
    
    * modal finished
    
    * fix modal
    
    * validation
    
    * Update works.css
    
    * Update works.html
    
    ---------
    
    Co-authored-by: nestertsovv <167287096+nestertsovv@users.noreply.github.com>

---
+++ src/js/createModal.js
@@ -0,0 +1,13 @@
+export function createModal(title, message) {
+  return `<div class="modal-message">
+    <button type="button" class="close-modal-btn js-modal-btn">
+      <svg class="modal-pict" width="24" height="24">
+        <use href="../img/icons.svg#icon-close"></use>
+      </svg>
+    </button>
+    <h4 class="modal-title">${title}</h4>
+    <p class="modal-descr">
+      ${message}
+    </p>
+  </div>`;
+}
