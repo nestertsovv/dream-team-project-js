0b13ba4f1aa90bcdc90b44ed46eab88e1ab35fb9
Oks <witchox@gmail.com>
2024-05-30 22:36:12 +0200

    done

---
+++ src/js/hero.js
@@ -0,0 +1,70 @@
+const colorsBG = {
+  olive: '#9db425',
+  orange: '#fe902b',
+  rosy: '#e64f79',
+  terracote: '#9a4360',
+  lilac: '#786bcb',
+  turquoise: '#005d7c',
+};
+
+const colorsText = {
+  olive: '#acc720',
+  orange: '#ff8514',
+  rosy: '#f35480',
+  terracote: '#9a4360',
+  lilac: '#9679f8',
+  turquoise: '#147da1',
+};
+
+const colorSelectorBtn = document.querySelector('.color-selector-btn');
+const ul = document.querySelector('.color-circles');
+const classBgColor = '.bg-color';
+const classTextColor = '.text-color';
+
+colorSelectorBtn.addEventListener('click', onSelectorClick);
+
+function onSelectorClick(e) {
+  console.log(colorSelectorBtn.classList);
+
+  colorSelectorBtn.classList.add('hidden');
+
+  console.log(colorSelectorBtn.classList);
+
+  console.log(ul.classList);
+
+  ul.classList.remove('hidden');
+
+  console.log(ul.classList);
+
+  document.addEventListener('mousedown', hideColors);
+}
+
+function hideColors(e) {
+  if (e.target.classList.contains('color-circle')) {
+    return;
+  }
+
+  ul.classList.add('hidden');
+  colorSelectorBtn.classList.remove('hidden');
+  document.removeEventListener('mousedown', hideColors);
+}
+
+ul.addEventListener('click', onColorClick);
+
+function onColorClick(e) {
+  if (e.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  const value = e.target.value;
+
+  const bgArr = document.querySelectorAll(classBgColor);
+  const colorArr = document.querySelectorAll(classTextColor);
+
+  bgArr.forEach(color => {
+    color.style.setProperty('--main-color', colorsBG[value]);
+  });
+
+  colorArr.forEach(color => {
+    color.style.setProperty('--main-color-text', colorsText[value]);
+  });
+}
