ac54c4d61fdcbffd50d6427bc6b8b49fec59ebea
Cheastear <56431973+Cheastear@users.noreply.github.com>
2024-05-31 15:20:15 +0300

    add padding on description+ render description (#26)
    * fix patch swaper + arrow cursor: pointer
    
    * fix + fix padding + margin fix
    
    * fix patch with width adaptive
    
    * fix
    
    * add id on section + add class to h3
    
    * add keyboard control
    
    * change id on section + fix press pageDown bug
    
    * add padding on description+ render description

--- src/js/about.js
+++ src/js/about.js
@@ -6,41 +6,97 @@ import { Navigation, Keyboard } from 'swiper/modules';
 
 import 'swiper/css/bundle';
 
+const description = document.querySelector('.description');
+
+const descriptionList = [
+  {
+    header: 'ABOUT ME',
+    descriptions: [
+      'I am Lloyd Jefferson, a talented programmer with extensive expertise in software development. I have an understanding of different programming languages ​​and technologies, always ready to improve my skills. I always follow the latest trends and look for unconventional, creative solutions to problems. I have a high level of analytical skills and am able to effectively solve even the most difficult tasks encountered on the way.',
+      'Able to work both independently and in a team. I can effectively cooperate with colleagues, exchanging ideas and finding optimal solutions. Professional maturity allows you to calmly cope with challenges and stressful situations, while maintaining a high quality of work. I am always looking for opportunities for self-improvement. I actively study new technologies and practices to stay abreast of the latest innovations. I have a strong understanding of various programming languages, frameworks and architectural concepts that allow me to create efficient and scalable software.',
+    ],
+  },
+  {
+    header: 'ROLE',
+    descriptions: [
+      'Frontend development',
+      'HeadlessCMS,Wordpress',
+      'Blender(enjoy)',
+    ],
+  },
+  {
+    header: 'EDUCATION',
+    descriptions: [
+      '2018 - 2019 / Frontend Development Diploma, GoIT IT School, New York',
+      '2019 - 2020 / Advanced Web Development Certificate, GoIT IT School, New York',
+      '2020 - 2022 / Advanced Blender Animation Techniques, Udemy',
+    ],
+  },
+];
+
+function init() {
+  descriptionList.forEach(elem => {
+    let innerHTML = '';
+
+    innerHTML += `<li class="description-li">
+                    <div class="description-item">
+                        <h3 class="section-name">${elem.header}</h3>
+                            <button class="description-button">
+                                <svg class="btn-svg" width="20" height="20">
+                                <use href="../img/icons.svg#icon-arrow"></use>
+                                </svg>
+                            </button>
+                            </div>
+                            <div class="description-content">
+                                <ul>`;
+    elem.descriptions.forEach(elem => {
+      innerHTML += `<li>${elem}</li>`;
+    });
+    innerHTML += `</ul>
+                </div>
+            </li>`;
+
+    description.innerHTML += innerHTML;
+  });
+}
+
+init();
+
 new Accordion('.description', {
-    elementClass: "description-li",
-    triggerClass: "description-item",
-    panelClass: "description-content",
-    activeClass: "svg-flip"
+  elementClass: 'description-li',
+  triggerClass: 'description-item',
+  panelClass: 'description-content',
+  activeClass: 'svg-flip',
 }).open(0);
 
-new Swiper(".languages", {
-    modules: [Navigation, Keyboard],
-    slidesPerView: 'auto',
-    freeMode: true,
-    loop: true,
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-        pageUpDown: false,
+new Swiper('.languages', {
+  modules: [Navigation, Keyboard],
+  slidesPerView: 'auto',
+  freeMode: true,
+  loop: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: false,
+  },
+  navigation: {
+    nextEl: '.languages-next-btn',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
     },
-    navigation: {
-        nextEl: ".languages-next-btn",
+    462: {
+      slidesPerView: 3,
     },
-    breakpoints: {
-        320: {
-            slidesPerView: 2,
-        },
-        462: {
-            slidesPerView: 3,
-        },
-        592: {
-            slidesPerView: 4,
-        },
-        768: {
-            slidesPerView: 3,
-        },
-        1440: {
-            slidesPerView: 6,
-        },
+    592: {
+      slidesPerView: 4,
     },
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
+  },
 });
