862c07851adcec1dfb855b3878f8cb7b675faa8b
Oleksandr_Zhynkin <godinsighted@gmail.com>
2024-06-01 21:05:13 +0300

    Validation/localstorage

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,11 +1,38 @@
-import { emailInput, messageInput, btnEl } from './workTogether';
+import {
+  emailInput,
+  messageInput,
+  btnEl,
+  formElem,
+  LS_KEY_FORM,
+} from './workTogether';
 
 let emailValue = null;
 let commentValue = null;
 const regPattern = /^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+let form = { email: '', comment: '' };
+
+export function onChangeForm(e) {
+  emailInput.addEventListener('focus', onFocusMail);
+  messageInput.addEventListener('focus', onFocusMessage);
+
+  const newForm = new FormData(formElem);
+  form.email = newForm.get('email').trim();
+  form.comment = newForm.get('comment').trim();
+
+  emailInput.addEventListener('blur', onValidationMail);
+  messageInput.addEventListener('blur', onValidationMessage);
+  const json = JSON.stringify(form);
+  localStorage.setItem(LS_KEY_FORM, json);
+
+  if (form.email && form.comment) {
+    btnEl.disabled = false;
+  } else {
+    btnEl.disabled = true;
+  }
+}
 
 export function onValidationMail(e) {
-  emailValue = e.target.value.trim();
+  emailValue = form.email;
 
   const validationEmail = e.target
     .closest('.small-text')
@@ -24,13 +51,15 @@ export function onValidationMail(e) {
   } else {
     onInvalid(validationEmail);
   }
+
   if (emailValue.length === 0) {
     initValid(validationEmail);
   }
 }
 
 export function onValidationMessage(e) {
-  commentValue = e.target.value.trim();
+  commentValue = form.comment;
+
   const validationMessage = e.target
     .closest('.small-text')
     .querySelector('.validation');
@@ -38,8 +67,8 @@ export function onValidationMessage(e) {
 
   messageInput.addEventListener('click', onClickCommentInput);
 
-  if (commentValue.length > 25) {
-    valueNotFocus = commentValue.slice(0, 25) + '...';
+  if (commentValue.length > 35) {
+    valueNotFocus = commentValue.slice(0, 35) + '...';
     e.target.value = valueNotFocus;
   }
 
@@ -48,20 +77,9 @@ export function onValidationMessage(e) {
   } else {
     onSuccess(validationMessage);
   }
+
   if (commentValue.length === 0) {
     initValid(validationMessage);
-  }
-}
-
-export function onChangeForm(e) {
-  emailInput.addEventListener('blur', onValidationMail);
-  messageInput.addEventListener('blur', onValidationMessage);
-  const email = emailInput.value;
-  const comment = messageInput.value;
-  if (email && comment) {
-    btnEl.disabled = false;
-  } else {
-    btnEl.disabled = true;
   }
 }
 
@@ -106,5 +124,29 @@ export function onClickEmailInput(e) {
 export function onClickCommentInput(e) {
   if (document.activeElement === e.target) {
     e.target.value = commentValue;
+  }
+}
+
+function onFocusMail(e) {
+  const validationMessage = e.target
+    .closest('.small-text')
+    .querySelector('.validation');
+  initValid(validationMessage);
+}
+function onFocusMessage(e) {
+  const validationMessage = e.target
+    .closest('.small-text')
+    .querySelector('.validation');
+  initValid(validationMessage);
+}
+
+export function renderPage() {
+  const lsData = localStorage.getItem(LS_KEY_FORM);
+  const result = JSON.parse(lsData);
+
+  if (result) {
+    formElem.elements.email.value = result.email;
+    formElem.elements.comment.value = result.comment;
+    btnEl.disabled = false;
   }
 }
