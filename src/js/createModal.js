3be43a3f14d6dd52c88b6a651af2aed33b82b414
nestertsovv <n.nestertsovv@gmail.com>
2024-06-03 13:24:11 +0300

    conflict fixes

--- src/js/createModal.js
+++ src/js/createModal.js
@@ -9,13 +9,7 @@ export async function createModal(title, message) {
     });
   return `<div class="modal-message">
     <button type="button" class="close-modal-btn js-modal-btn">
-<<<<<<< Updated upstream
-      <svg class="modal-pict" width="24" height="24">
-        <use href="/img/icons.svg#icon-close"></use>
-      </svg>
-=======
       ${svgData}
->>>>>>> Stashed changes
     </button>
     <h4 class="modal-title">${title}</h4>
     <p class="modal-descr">
